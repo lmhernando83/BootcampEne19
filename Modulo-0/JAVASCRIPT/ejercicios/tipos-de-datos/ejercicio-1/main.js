5f3d8895aae8f74c73f700dfd5fc2c7cde77340b
lmhernando83 <lmhernando83@gmail.com>
2018-12-17 20:11:57 +0100

    ejercicios/tipos-de-datos/ejercicio-1

---
+++ Modulo-0/JAVASCRIPT/ejercicios/tipos-de-datos/ejercicio-1/main.js
@@ -0,0 +1,10 @@
+
+let diasSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+
+alert(diasSemana[0]);
+alert(diasSemana[1]);
+alert(diasSemana[2]);
+alert(diasSemana[3]);
+alert(diasSemana[4]);
+alert(diasSemana[5]);
+alert(diasSemana[6]);
