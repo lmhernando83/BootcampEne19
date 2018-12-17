adf388fe7e2a6a12244dbb4b9fa13a96f8bb15a6
lmhernando83 <lmhernando83@gmail.com>
2018-12-17 19:23:11 +0100

    ejercicios/mi-primer-script/ejercicio-3

---
+++ Modulo-0/JAVASCRIPT/ejercicios/mi-primer-script/ejercicio-3/main.js
@@ -0,0 +1,9 @@
+/* No se visualiza nada del HTML
+   sin antes ver los alerts
+*/
+// Primer Mensaje de JavaScript
+alert('Mi primer script');
+// Segundo mensaje de JavaScript
+alert('Bienvenido al curso de JavaScript')
+// Mensaje en la consolo del Navegador
+console.log('Mi primer script');
