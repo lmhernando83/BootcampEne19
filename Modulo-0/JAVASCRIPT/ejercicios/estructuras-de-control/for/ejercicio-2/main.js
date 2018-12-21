d3cdf0d718b9e959c5fa12cd4a04f4934f581162
lmhernando83 <lmhernando83@gmail.com>
2018-12-21 21:46:15 +0100

    ejercicios/estructuras-de-control/for/ejercicio2

---
+++ Modulo-0/JAVASCRIPT/ejercicios/estructuras-de-control/for/ejercicio-2/main.js
@@ -0,0 +1,5 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+for(let i = 0; i < meses.length; i++){
+    alert(meses[i]);
+}
