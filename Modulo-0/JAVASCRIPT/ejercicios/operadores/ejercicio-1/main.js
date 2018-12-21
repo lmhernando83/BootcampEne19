5ad128fa5aa75d6e086a79dfad00de3b1a103814
lmhernando83 <lmhernando83@gmail.com>
2018-12-21 15:19:54 +0100

    ejercicios/operadores/ejercicio-1

---
+++ Modulo-0/JAVASCRIPT/ejercicios/operadores/ejercicio-1/main.js
@@ -0,0 +1,32 @@
+const myArray = [true, 'Curso', 65, 4, 'javaScript', false];
+
+alert(myArray[4]);
+
+// Operaciones Matematicas
+alert(myArray[2] + myArray[3]);
+alert(myArray[2] - myArray[3]);
+alert(myArray[2] * myArray[3]);
+alert(myArray[2] / myArray[3]);
+alert(myArray[2] ** myArray[3]);
+alert(myArray[2] % myArray[3]);
+
+// Asignaciones logicas true
+alert(myArray[0] && !myArray[5]);
+alert(myArray[0] || myArray[5]);
+alert(myArray[0] || !myArray[5]);
+alert(!myArray[0] || !myArray[5]);
+alert(myArray[0] != myArray[5]);
+alert(myArray[0] !== myArray[5]);
+
+// Asignaciones logicas false
+alert(myArray[0] && myArray[5]);
+alert(!myArray[0] && !myArray[5]);
+alert(!myArray[0] && myArray[5]);
+alert(!myArray[0] || myArray[5]);
+alert(myArray[0] == myArray[5]);
+alert(myArray[0] === myArray[5]);
+
+
+
+
+
