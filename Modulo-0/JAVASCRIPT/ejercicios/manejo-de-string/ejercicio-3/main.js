773e9524613d83cc676affa8d324bfcf552d4a3a
lmhernando83 <lmhernando83@gmail.com>
2018-12-22 18:52:23 +0100

    ejercicios/manejo-de-strings/reemplazar/ejercicio1

---
+++ Modulo-0/JAVASCRIPT/ejercicios/manejo-de-string/ejercicio-3/main.js
@@ -0,0 +1,13 @@
+let cadena = 'JavaScript es un lenguaje de programación que nacio en 1995. JavaScript no tiene que ver en nada con Java';
+
+let buscar = 'JavaScript';
+
+let pos = cadena.indexOf(buscar);
+
+while (pos !== -1) {
+    cadena = cadena.replace(buscar, 'JAVASCRIPT');
+    pos = cadena.indexOf(buscar, pos + 1);
+}
+
+console.log('Resultado: ', cadena);
+
