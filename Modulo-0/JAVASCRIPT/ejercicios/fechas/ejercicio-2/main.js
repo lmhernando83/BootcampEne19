aa286af5666135d997907832710e192649f4e4ec
lmhernando83 <lmhernando83@gmail.com>
2018-12-28 17:05:31 +0100

    ejercicios/fechas/ejercicio2

---
+++ Modulo-0/JAVASCRIPT/ejercicios/fechas/ejercicio-2/main.js
@@ -0,0 +1,35 @@
+function fecha(dia, mes, ano) {
+    
+    if (ano <= 0) {
+        return false;
+    }
+    if (mes < 0 || mes > 11) {
+        return false;
+    }
+    if (dia < 1 || dia > 31) {
+        return false;
+    }
+    
+    if (!(ano % 4 === 0 && ano % 100 !== 0) && mes === 1 && dia >= 29) {
+        return false;
+    }
+  
+    if ((mes === 1 || mes === 3 || mes === 5 || mes === 8 || mes === 10 ) && dia >= 30) {
+        return false;
+    }
+
+    return new Date(ano, mes, dia);
+  
+}
+
+console.log(fecha(28,1,2019));
+
+function validar(fecha){
+    
+    let parte = fecha.split('/');
+
+   return fecha(parseInt(partes[0]), parseInt(partes[1]), parseInt(partes[2]));
+
+}
+
+console.log(fecha(28/1/2019) === false);
